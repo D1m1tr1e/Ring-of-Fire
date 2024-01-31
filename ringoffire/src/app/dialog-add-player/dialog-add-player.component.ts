45e5ed9e9097ef87b7bef5a8188e2ccf15462822
D1m1tr1e <126110046+D1m1tr1e@users.noreply.github.com>
2024-01-31 22:02:00 +0100

    finished dialog window

--- ringoffire/src/app/dialog-add-player/dialog-add-player.component.ts
+++ ringoffire/src/app/dialog-add-player/dialog-add-player.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatDialogClose } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dialog-add-player',
   standalone: true,
-  imports: [],
+  imports: [MatInputModule, CommonModule, FormsModule, MatDialogClose],
   templateUrl: './dialog-add-player.component.html',
   styleUrl: './dialog-add-player.component.scss'
 })
 export class DialogAddPlayerComponent {
+
+  name: string = '';
+
+  onNoClick() { }
 
 }
