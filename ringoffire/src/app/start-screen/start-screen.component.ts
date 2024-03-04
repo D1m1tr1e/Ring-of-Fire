a4d14e8aa0595396aac42a5022f897607097836a
D1m1tr1e <126110046+D1m1tr1e@users.noreply.github.com>
2024-03-04 22:28:32 +0100

    .

--- ringoffire/src/app/start-screen/start-screen.component.ts
+++ ringoffire/src/app/start-screen/start-screen.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore';
+import { Game } from '../models/game';
+import { addDoc, collection } from '@firebase/firestore';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-start-screen',
@@ -8,5 +13,24 @@ import { Component } from '@angular/core';
   styleUrl: './start-screen.component.scss'
 })
 export class StartScreenComponent {
+
+  game!: Game;
+  firestore: Firestore = inject(Firestore);
+
+  constructor(private router: Router) {
+  }
+
+  async newGame() {
+    this.game = new Game();
+    await addDoc(this.getGameColRef(), this.game.toJson()).then((gameInfo: any) => {
+      this.router.navigateByUrl('/game/' + gameInfo.id);
+      console.log(gameInfo);
+    });
+
+  }
+
+  getGameColRef() {
+    return collection(this.firestore, 'games');
+  }
 
 }
